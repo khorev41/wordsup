12b9d59bb3cb1de83b238a729c517da70df84c23
Vasyl <khorev.41@gmail.com>
2023-06-14 17:22:54 +0200

    corrected quizlogic changed database model updated definitions model

--- app/src/main/java/sk/upjs/wordsup/dao/WordsDatabase.kt
+++ app/src/main/java/sk/upjs/wordsup/dao/WordsDatabase.kt
@@ -3,6 +3,11 @@ package sk.upjs.wordsup.dao
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import sk.upjs.wordsup.dao.quiz.Quiz
+import sk.upjs.wordsup.dao.quiz.QuizDao
+import sk.upjs.wordsup.dao.quiz.QuizWordCrossRef
+import sk.upjs.wordsup.dao.word.Word
+import sk.upjs.wordsup.dao.word.WordsDao
 
 @Database(entities = [Word::class, Quiz::class, Try::class, QuizWordCrossRef::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
